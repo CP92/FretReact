275e0f2b28c924909b49e06b7a89c835a90ac770
CP92 <brd4jc@gmail.com>
2018-12-26 11:18:41 -0500

    Added remove and add string buttons, added colors to notes
    Add string button adds an additional string that starts a fourth lower
    than the lowest string.

--- src/FretBoard.js
+++ src/FretBoard.js
@@ -7,6 +7,7 @@ import MenuItem from 'react-bootstrap/lib/MenuItem'
 
 
 const noteToolBox = require('./notes.js')
+let context
 class FretBoard extends Component {
   constructor (props) {
   	super(props)
@@ -15,23 +16,54 @@ class FretBoard extends Component {
       notes: props.notes
     }
 
+    this.fretClick = this.fretClick.bind(this)
     this.changeRow = this.changeRow.bind(this)
+    this.loadAudio = this.loadAudio.bind(this)
+    this.addString = this.addString.bind(this)
+    this.removeString = this.removeString.bind(this)
 
   }
 
+  loadAudio () {
+    try {window.AudioContext = window.AudioContext || window.webkitAudioContext
+      context = new AudioContext()
+    }
+    catch (e) {
+
+    }
+  }
+
+  fretClick (e) {
+    console.log(e.target.innerText)
+  }
+
   changeRow (e) {
-    
     const note = e.slice(2)
     const row = e.slice(0,1)
-    console.log(note)
-    console.log(row)
     const newNoteRow = noteToolBox.getRowNotes(note)
-    console.log(newNoteRow)
     const set = this.state.notes
     set[row] = newNoteRow
     this.setState((state, props) => ({
       notes: set
     }))
+  }
+
+  addString (e) {
+    const lowestOpenNote = this.state.notes[this.state.notes.length - 1][0]
+    const set = this.state.notes
+    set.push(noteToolBox.getRowNotes(noteToolBox.getFourth(lowestOpenNote)))
+    console.log(set)
+    this.setState((state, props) => ({
+      notes: set
+    }))
+  }
+
+  removeString (e) {
+    const set = this.state.notes
+    set.pop()
+    this.setState((state, props) => ({
+      notes: set
+    })) 
   }	
 
 
@@ -41,13 +73,14 @@ class FretBoard extends Component {
     return (
       <div>
       	<h1>{title}</h1>
-      	<Grid>
+      	<Grid className="fretboard">
           {this.state.notes.map((set, rowIndex) => (
             <Row key={`row-${rowIndex}`} id={`row-${rowIndex}`} className={`text-center show-grid ${rowIndex === this.state.notes.length - 1 ? 'last-row' : ''}` }>
               {set.map((note, colIndex) => (
-                <Col key={`fret-${rowIndex}-${colIndex}`} id={`fret-${rowIndex}-${colIndex}`} sx={1} className={colIndex === 0 ? 'tuner-box box text-center' : 'box text-center'}>
+                <Col key={`fret-${rowIndex}-${colIndex}`} onClick={colIndex !== 0 ? this.fretClick : null} id={`fret-${rowIndex}-${colIndex}`} sx={1} className={colIndex === 0 ? 'tuner-box box text-center' : 'box text-center'}>
+                  {colIndex !== 0 ? <hr className="text-center center-block"/> : null}
                   {colIndex === 0 ? 
-                    <DropdownButton noCaret title={note} key={`tuner-${rowIndex}`} id={`tuner-${rowIndex}`} class="text-center center-block">
+                    <DropdownButton noCaret title={note} key={`tuner-${rowIndex}`} id={`tuner-${rowIndex}`} className="text-center center-block">
                       <MenuItem eventKey={note} active >{note}</MenuItem>
                       <MenuItem onSelect={this.changeRow} eventKey={rowIndex + '-A '} >A</MenuItem>
                       <MenuItem onSelect={this.changeRow} eventKey={rowIndex + '-A#'} >A#</MenuItem>
@@ -61,14 +94,24 @@ class FretBoard extends Component {
                       <MenuItem onSelect={this.changeRow} eventKey={rowIndex + '-F#'} >F#</MenuItem>
                       <MenuItem onSelect={this.changeRow} eventKey={rowIndex + '-G '} >G</MenuItem>
                       <MenuItem onSelect={this.changeRow} eventKey={rowIndex + '-G#'} >G#</MenuItem>
-                    </DropdownButton> : note}
+                    </DropdownButton> : <span className="note">{note}</span>  }
+                  {colIndex !== 0 ? <hr className="text-center center-block"/> : null}  
                 </Col>
               ))} 
             </Row>
           ))}
+          
         </Grid>
+
+        <button onClick={this.addString} type="button" className="btn btn-success btn-sm" id="add-string">
+          <span className="fas fa-plus-square" aria-hidden="true"></span> Add String
+        </button>
+        <button onClick={this.removeString} type="button" className="btn btn-danger btn-sm float-right" id="remove-string">
+          <span className="fas fa-minus-square" aria-hidden="true"></span> Remove String
+        </button>
       	
       	
+
       </div>
     )
   }
--- src/notes.js
+++ src/notes.js
@@ -10,11 +10,24 @@ const getRowNotes = function (note) {
   		const index = (i + musicNotes.indexOf(note)) % musicNotes.length
   		outNotes.push(musicNotes[index])
   	}
-  	
   	return outNotes		 
   }
 }
 
+const getFourth = function (note) {
+  //note = note.replace(/\s/g, '')
+  //console.log(note)
+  if (musicNotes.indexOf(note) - 5 < 0) {
+    //console.log(musicNotes[((musicNotes.indexOf(note) - 5) + 12)])
+    return musicNotes[((musicNotes.indexOf(note) - 5) + 12)]
+  } else {
+    //console.log(musicNotes[musicNotes.indexOf(note) - 5])
+    return musicNotes[musicNotes.indexOf(note) - 5]
+  }
+
+}
+
 module.exports = {
-  getRowNotes
+  getRowNotes,
+  getFourth
 }
